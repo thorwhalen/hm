12414b9bdda9ce72796627b695b151823a183414
Thor Whalen <thorwhalen1@gmail.com>
2022-09-29 17:50:21 +0200

    0.0.3:

--- hm/phonemes.py
+++ hm/phonemes.py
@@ -28,8 +28,8 @@ def _text_to_term_and_phones(text):
 
 from functools import cached_property
 from operator import methodcaller
-from itertools import chain
-from collections import Counter
+from itertools import chain, product
+from collections import Counter, defaultdict
 
 
 class Phone:
@@ -65,10 +65,85 @@ class Phone:
         return sorted(self.phone_counts)
 
 
+def keys_for_value(d: dict):
+    """An inverse of dict ``d``.
+    If ``d`` is a mapping of ``(key, value)``, ``keys_for_value`` will return
+    a dict of ``(unique_value, group_of_key)`` items.
+    """
+    inverse_d = defaultdict(list)
+
+    for key, value in d.items():
+        inverse_d[value].append(key)
+
+    return dict(inverse_d)
+
+
 class MST(Phone):
     """
     See:    https://en.wikipedia.org/wiki/Mnemonic_major_system
 
     """
 
-    phones_for_num = {0: {'Z'}}
+    phones_for_num = {
+        0: {'Z', 'S'},
+        1: {'T', 'D', 'TH', 'DH'},
+        2: {'N'},
+        3: {'M'},
+        4: {'R'},
+        5: {'L'},
+        6: {'CH', 'SH', 'JH'},
+        7: {'K', 'G'},
+        8: {'F', 'V'},
+        9: {'P', 'B'},
+    }
+
+    @cached_property
+    def num_of_phone(self):
+        def gen():
+            for num, v in self.phones_for_num.items():
+                for phone in v:
+                    yield phone, num
+
+        return dict(sorted(gen()))
+
+    @cached_property
+    def mst_phones(self):
+        return set(self.num_of_phone)
+
+    def term_to_mst_sequence(self, sentence):
+        terms = map(str.lower, sentence.split())
+        phones = chain.from_iterable(map(self.tp.get, terms))
+        return list(filter(self.mst_phones.__contains__, phones))
+
+    def term_to_nums(self, sentence):
+        return list(map(self.num_of_phone.get, self.term_to_mst_sequence(sentence)))
+
+    @property
+    def nums_of_term(self):
+        return {term: self.term_to_nums(term) for term in self.tp}
+
+    @cached_property
+    def numstr_of_term(self):
+        return {
+            term: ''.join(map(str, nums)) for term, nums in self.nums_of_term.items()
+        }
+
+    @cached_property
+    def terms_of_numstr(self):
+        return keys_for_value(self.numstr_of_term)
+
+    def num_to_terms(self, num):
+        def _middle_out_sort(a):
+            return sorted(a, key=lambda x: abs(x - len(a) / 2))
+
+        if not isinstance(num, str):
+            num = str(num)
+        if num in self.terms_of_numstr:
+            yield from self.terms_of_numstr[num]
+        for i in _middle_out_sort(range(0, len(num))):
+            if 0 < i < len(num) - 1:
+                # print(i)
+                first_half, second_half = num[:i], num[i:]
+                yield from product(
+                    self.num_to_terms(first_half), self.num_to_terms(second_half)
+                )
